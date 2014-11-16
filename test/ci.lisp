dbc1d0e60fdb31c0a0dd7030d129ce4ef08b4634
Deepak Surti <dmsurti@gmail.com>
2014-11-16 17:42:05 +0530

    API to run tests and collect test suite status

--- test/ci.lisp
+++ test/ci.lisp
@@ -3,9 +3,21 @@
 ;;; Test operation to test entire NLP test suite
 ;;; This test operation is also used by Travis CI
 
+(defparameter *test-failures-or-errors* 0)
+
 (defmethod asdf:perform ((o asdf:test-op)
                          (s (eql (asdf:find-system :cl-nlp))))
   (asdf:load-system :cl-nlp)
-  ;;; TO DO: Fix and add other package tests
-  (every #'identity (mapcar #'(lambda (pkg) (should-test:test :package pkg))
-                            '(:ncore :ncorp :nlearn :ntag :nlp-user))))
+  (run-all-tests)
+  (test-suite-status))
+
+(defun run-all-tests ()
+  "Run all test modules and keep track of number of failures/errors."
+  (setf *test-failures-or-errors* 0)
+  (dolist (package '(:ncore :ncorp :nlearn :ntag :nlp-user))
+    (unless (should-test:test :package package)
+      (incf *test-failures-or-errors* 1))))
+
+(defun test-suite-status ()
+  "Used by CI as unix command status."
+  (values *test-failures-or-errors*))
